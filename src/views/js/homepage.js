68e400c2ea98074a5f51a15323b9ac8098d77869
AguuSz <agus.sepu2000@hotmail.com>
2019-07-24 14:59:55 -0300

    Indicacion de estado de app

--- src/views/js/homepage.js
+++ src/views/js/homepage.js
@@ -4,66 +4,70 @@ const url = require('url');
 const swal = require('sweetalert2');
 const fs = require('fs');
 const ps = require('python-shell');
-const dirname = path.join(__dirname, '../', 'scripts/', 'faces/')
+const dirname = path.join(__dirname, '../', 'scripts/', 'faces/');
+const estado = document.getElementById('estado');
 
 const iniciar = document.getElementById('iniciarBtn');
-iniciar.addEventListener('click', function(event) {
-    
-    var options = {
-        scriptPath: path.join(__dirname, "../", "scripts"),
-        pythonPath: "/home/agus/.local/share/virtualenvs/facialRecog-OpenCV-SWeLqkmQ/bin/python"
-    }
+iniciar.addEventListener('click', function (event) {
+	var options = {
+		scriptPath : path.join(__dirname, '../', 'scripts'),
+		pythonPath : '/home/agus/.local/share/virtualenvs/facialRecog-OpenCV-SWeLqkmQ/bin/python'
+	};
 
-    ps.PythonShell.run('app.py', options, function(err, results) {
-        if(err) throw err;
-    });
-})
+	ps.PythonShell.run('app.py', options, function (err, results) {
+		if (err) throw err;
+		// Cuando se cierre la app, que pase esto
+		estado.innerHTML = 'Offline';
+	});
+
+	// Cuando se abre la app, que pase esto
+	estado.innerHTML = 'Online';
+});
 const añadirBtn = document.getElementById('añadirBtn');
-añadirBtn.addEventListener('click', function(event) {
-
-    addNewFace = new remoteWindow.BrowserWindow({
-        width: 450,
-        height: 300,
-        frame: false,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
-    addNewFace.loadURL(url.format({
-        pathname: path.join(__dirname, 'addNewFace.html'),
-        protocol: 'file',
-        slashes: true
-    }));
-    addNewFace.on('closed', () => {
-        addNewFace = null;
-    })
-})
+añadirBtn.addEventListener('click', function (event) {
+	addNewFace = new remoteWindow.BrowserWindow({
+		width          : 450,
+		height         : 300,
+		frame          : false,
+		webPreferences : {
+			nodeIntegration : true
+		}
+	});
+	addNewFace.loadURL(
+		url.format({
+			pathname : path.join(__dirname, 'addNewFace.html'),
+			protocol : 'file',
+			slashes  : true
+		})
+	);
+	addNewFace.on('closed', () => {
+		addNewFace = null;
+	});
+});
 const borrarBtn = document.getElementById('borrarBtn');
-borrarBtn.addEventListener('click', function(event) {
-    
-    var ruta = path.join(__dirname, '..', 'scripts', 'faces');
-    try {
-        if (!fs.existsSync(ruta)) {
-            fs.mkdirSync(ruta)
-        }
-    } catch(err) {
-        console.error(err)
-    }
-    swal.fire(
-        'Instrucciones',
-        'Se abrirá el directorio, lo unico que debe hacer para eliminar a una persona es borrar su foto.',
-        'question'
-    )
-    setTimeout(openDir, 3000);
-    
-})
+borrarBtn.addEventListener('click', function (event) {
+	var ruta = path.join(__dirname, '..', 'scripts', 'faces');
+	try {
+		if (!fs.existsSync(ruta)) {
+			fs.mkdirSync(ruta);
+		}
+	} catch (err) {
+		console.error(err);
+	}
+	swal.fire(
+		'Instrucciones',
+		'Se abrirá el directorio, lo unico que debe hacer para eliminar a una persona es borrar su foto.',
+		'question'
+	);
+	setTimeout(openDir, 3000);
+});
 
 const salirBtn = document.getElementById('salirBtn');
-salirBtn.addEventListener('click', function(event) {
-    var window = remoteWindow.getCurrentWindow();
-    window.close();
-})
+salirBtn.addEventListener('click', function (event) {
+	var window = remoteWindow.getCurrentWindow();
+	window.close();
+});
 
-function openDir() {
-    remoteWindow.shell.openItem(path.join(__dirname, '..', 'scripts', 'faces'))
+function openDir () {
+	remoteWindow.shell.openItem(path.join(__dirname, '..', 'scripts', 'faces'));
 }
